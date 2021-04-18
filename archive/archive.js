622ba1a323a4e20c47ddcd9f4c9e8f00cb7eb30e
Rafael Gonçalves <rafaelgoncalves@esad.pt>
2021-04-18 16:51:21 +0100

    Fix image sizes and add image Hover

--- archive/archive.js
+++ archive/archive.js
@@ -11,6 +11,7 @@ for (let i = 0; i < databaseArchive.length; i++) {
 
 
 function renderGrid(specie) {
+    const imageUrl = "../images/species/" + specie.NAME.toLowerCase() + ".jpeg"
     const htmlTemplate = `
         <tr class="grid grid-9 margin">
             <td class="type-monospaced monospaced-small">${specie.NUMBER}</td>
@@ -23,6 +24,7 @@ function renderGrid(specie) {
             <td class="type-monospaced monospaced-small">${specie.SUBORDER}</td>
             <td class="type-monospaced monospaced-small">${specie.FAMILY}</td>
         </tr>
+        <img class="fullscreen-image" src="${imageUrl}"> 
     `;
 
     const table = document.getElementById("species-table")
